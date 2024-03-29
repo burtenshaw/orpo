f5934eb1fe1f4610861752407bdf0bcd86f2c0c1
burtenshaw <ben.burtenshaw@gmail.com>
2024-03-29 09:46:38 +0000

    add samples array

--- runpod.sh
+++ runpod.sh
@@ -5,20 +5,29 @@ sed -i 's/num_processes: 2/num_processes: 1/' ./src/accelerate/fsdp.yaml
 sed -i 's/--num_proc", default=8/--num_proc", default=1/' ./src/args.py
 wandb login $WANDB_TOKEN
 wandb init -p $WANDB_PROJECT
-accelerate launch --config_file ./src/accelerate/fsdp.yaml main.py \
-    --lr $LEARNING_RATE \
-    --warmup_steps 100 \
-    --model_name $MODEL_ID \
-    --data_name $DATASET \
-    --num_train_epochs $EPOCH \
-    --max_samples $MAX_SAMPLES \
-    --prompt_max_length 128 \
-    --response_max_length 2048 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
-    --num_proc 1
-cd $OUTPUT
-cd */
-huggingface-cli login --token $TOKEN
-huggingface-cli upload $NEW_MODEL . .
+
+# iterate over split max samples on commas into ints
+for i in $(echo $MAX_SAMPLES | sed "s/,/ /g")
+do
+    echo "Running with $i samples"
+    
+    accelerate launch --config_file ./src/accelerate/fsdp.yaml main.py \
+        --lr $LEARNING_RATE \
+        --warmup_steps 100 \
+        --model_name $MODEL_ID \
+        --data_name $DATASET \
+        --num_train_epochs $EPOCH \
+        --max_samples $i \
+        --prompt_max_length 128 \
+        --response_max_length 2048 \
+        --per_device_train_batch_size 4 \
+        --per_device_eval_batch_size 4 \
+        --gradient_accumulation_steps 1 \
+        --num_proc 1
+    cd $OUTPUT
+    cd */
+    huggingface-cli login --token $TOKEN
+    huggingface-cli upload $NEW_MODEL . .    
+done
+
+
